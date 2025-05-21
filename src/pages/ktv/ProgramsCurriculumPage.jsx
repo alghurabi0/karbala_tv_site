a4b24aa6a1bfb447b7f1e64df1b42391387149b8
alghurabi0 <zaid.alghurabi01@gmail.com>
2025-05-21 18:11:07 +0300

    active program

--- src/pages/ktv/ProgramsCurriculumPage.jsx
+++ src/pages/ktv/ProgramsCurriculumPage.jsx
@@ -9,6 +9,7 @@ function ProgramsCurriculumPage() {
       id: 1,
       time: "00:00",
       image: "/images/karbala_city.jpg",
+      active: false,
       title: "نور البيان",
       description:
         "برنامج حواري عقائدي الذي يتضمن في محتواه التدبر والتأمل في النصوص القرآنية المتضمنة للأحداث والوقائع القصصية ونحو ذلك بصحبة نخبة من ضيوف المتواجدين في كل حلقة من حلقات البرنامج",
@@ -17,6 +18,7 @@ function ProgramsCurriculumPage() {
       id: 2,
       time: "01:00",
       image: "/images/karbala_city.jpg",
+      active: false,
       title: "الاخبار",
       description: "اخبار منوعة",
     },
@@ -24,6 +26,7 @@ function ProgramsCurriculumPage() {
       id: 3,
       time: "02:00",
       image: "/images/karbala_city.jpg",
+      active: true,
       title: "الدين والحياة",
       description:
         "برنامج يوضح الارتباط الوثيق للدين في حياة الانسان وتبيان الشريعة التي شرعها الله جل وعلا للبشر لهدايتهم، يتطرق فيه سماحة الشيخ محمد كنعان الى موضوع عقائدي ثم يتسلم اسئلة المشاهدين في الامور الفقهية والعقائدية ليجيب عليها. وقت البرنامج ساعة ونصف يبث بشكل مباشر يوم الاثنين ويعاد يوم الثلاثاء الساعة الواحدة والنصف صباحا",
@@ -32,6 +35,7 @@ function ProgramsCurriculumPage() {
       id: 4,
       time: "03:00",
       image: "/images/karbala_city.jpg",
+      active: false,
       title: "سراج المودة",
       description:
         "برنامج اجتماعي ثقافي تربوي يسلط الضوء في كل حلقة من حلقاته على موضوع ليفتح له باباً من ابواب السعادة الزوجية بحوار يجمع الخبراء والمختصين لمناقشته من اجل تعزيز التواصل السليم في اسرار السعادة الزوجية",
@@ -40,6 +44,7 @@ function ProgramsCurriculumPage() {
       id: 5,
       time: "04:00",
       image: "/images/karbala_city.jpg",
+      active: false,
       title: "ادعية وزيارات عامة",
       description: "",
     },
@@ -47,6 +52,7 @@ function ProgramsCurriculumPage() {
       id: 6,
       time: "05:00",
       image: "/images/karbala_city.jpg",
+      active: false,
       title: "موعد صلاة الصبح",
       description: "",
     },
@@ -68,11 +74,14 @@ export default ProgramsCurriculumPage;
 function ProgramCurriculumTile({ program }) {
   return (
     <>
-      <div className="program">
+      <div className={"program" + (program.active ? " active" : "")}>
         <section className="time">
-          <span className="time-text">{program.time}</span>
-          <FaPlay color="grey" size={20} />
-          <CiTimer size={25} color="red" />
+          {program.active ? <div className="live-text">الان</div> : ""}
+          <div className="time-container">
+            <span className="time-text">{program.time}</span>
+            <FaPlay className="play-logo" size={20} />
+            <CiTimer className="clock-logo" size={25} />
+          </div>
         </section>
         <img src={program.image} className="img" />
         <section className="metadata">
